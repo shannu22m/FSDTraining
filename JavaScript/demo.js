5c3b9c886eb5a350b235a068da25c2cc00633231
shannu22m <shanmukhamandapalli22@gmail.com>
2026-06-11 15:34:14 +0530

    Initial JavaScript Commit

--- JavaScript/demo.js
+++ JavaScript/demo.js
@@ -1,21 +1,34 @@
-function fetchUserName(){
-    return new Promise((resolve,reject)=>{
+function fetchUserName() {
+    return new Promise((resolve, reject) => {
         const userName = "shanmukha";
         resolve(userName);
     })
 }
-function fetchUserDetails(userName){
-    const user={
-        name:"M.Shanmukha",
-        roll:"23B81A6646"
+function fetchUserDetails(userName) {
+    const user = {
+        name: "M.Shanmukha",
+        roll: "23B81A6646"
     };
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
         resolve(user);
     })
 }
+
+function displayDetails(userDetails){
+    return new Promise((resolve,reject)=>{
+        console.log(userDetails.name);
+        console.log(userDetails.roll);
+        resolve("Success");
+    })
+}
 fetchUserName(1)
-.then(response=>{
-    console.log(response);
-    return fetchUserDetails(response);
-}).then(result=>console.log(result))
-.catch(error=>console.log(error));
+    .then(response => {
+        console.log(response);
+        return fetchUserDetails(response);
+    }).then(result => {
+        console.log(result);
+        return displayDetails(result);
+    }).then((output)=>console.log(output))
+
+
+
