cc4cc88d99d8d549b737481f11afe3bbc50e1b06
shannu22m <shanmukhamandapalli22@gmail.com>
2026-06-16 15:32:23 +0530

    HTTP server created

--- Node/server.js
+++ Node/server.js
@@ -1,8 +1,30 @@
-const http=require("http");
-const server=http.createServer((req,res)=>{
-    res.write("<h1><b>hello</b></h1>");
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url == "/" && req.method == "GET") {
+    res.write(JSON.stringify(getData()));
     res.end();
-})
-server.listen(3000,()=>{
-    console.log("server listening on http://localhost:3000");
-})
+  }
+  if (req.url == "/" && req.method == "POST") {
+    const user = req.body;
+    let data = getData();
+    data.users.push(user);
+    saveData(data);
+
+    res.end("Added Successfully");
+  }
+});
+
+const fs = require("fs");
+
+function getData() {
+  const data = fs.readFileSync("./db.json", "utf8");
+  return JSON.parse(data);
+}
+
+function saveData(data) {
+  fs.writeFileSync("./db.json", JSON.stringify(data), "utf8");
+}
+
+server.listen(3000, () => {
+  console.log("server listening on http://localhost:3000");
+});
